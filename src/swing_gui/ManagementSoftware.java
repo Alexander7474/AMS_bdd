a1baf7f293e7c6d4f4245d281bd123ed6d2b64ce
Florent <florent.cagnard@gmail.com>
2025-01-03 21:22:24 +0100

    Suppression de l'onglet ventes pour le mettre dans inventaire

--- src/swing_gui/ManagementSoftware.java
+++ src/swing_gui/ManagementSoftware.java
@@ -18,27 +18,28 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ManagementSoftware {
-	
+
 	private static JPanel sidebar;
 	private static JFrame frame;
 	private static Theme theme = Theme.LIGHT; // pas encore implémenté
 
 	public static void loadMainApplication(String username) {
-		
+
 		// ==============================================================
 		ManagementSoftware.clearApp();
 		Gestion.appStart();
-		
+
 		// ==============================================================
-		
-		// On créer la frame et la sidebar si ce n'est pas fais
+
+		// Création de la frame et de la sidebar
+
 		getFrame();
 		getSidebar();
 
 		// ==============================================================
-		// Création de l'affichage du profil utilisateur 
+
+		// Création de l'affichage du profil utilisateur
 
 		// Affichage de l'utilisateur connecté
 		JLabel userLabel = ManagementSoftware.getSidebarText("Utilisateur : " + username);
@@ -47,19 +48,17 @@ public class ManagementSoftware {
 		JLabel roleLabel = ManagementSoftware.getSidebarText("Rôle : " + UserManager.getUserRole(username));
 
 		// ==============================================================
-		// Création des boutons 
 
-		// Bouton pour accéder à la gestion des fournisseurs
-		JButton inv = ManagementSoftware.getSidebarButton("Inventaire/Lots");
+		// Création des boutons
+
+		// Bouton pour accéder à l'inventaire du magasin et au mode caisse
+		JButton inv = ManagementSoftware.getSidebarButton("Inventaire/Lots & Caisse");
 
 		// Bouton pour accéder à la gestion des fournisseurs
 		JButton suppliers = ManagementSoftware.getSidebarButton("Fournisseurs/Contacts");
 
 		// Bouton pour accéder à la gestion des produits
 		JButton products = ManagementSoftware.getSidebarButton("Fournisseurs/Produits");
-
-		// Bouton pour accéder à la gestion des ventes
-		JButton sales = ManagementSoftware.getSidebarButton("Ventes");
 
 		// Bouton pour accéder à la gestion des contrats
 		JButton contracts = ManagementSoftware.getSidebarButton("Contrats");
@@ -69,10 +68,10 @@ public class ManagementSoftware {
 
 		// Bouton pour accéder aux paramètres de l'application
 		JButton stats = ManagementSoftware.getSidebarButton("Statistiques");
-				
+
 		// Bouton pour accéder aux paramètres de l'application
 		JButton parameters = ManagementSoftware.getSidebarButton("Paramètres");
-		
+
 		// ==============================================================
 
 		// Bouton pour se déconnecter
@@ -99,18 +98,20 @@ public class ManagementSoftware {
 		contentPanel.add(titleLabel);
 
 		// ==============================================================
-		//inventaire
-		
+
+		// Onglet des inventaires / lots + format caisse
+
 		inv.addActionListener(e -> {
 
 			contentPanel.removeAll();
 
 			InvTab.loadInvTab(contentPanel, frame);
-			
+
 		});
 		// ==============================================================
+
 		// Onglet des fournisseurs
-		
+
 		suppliers.addActionListener(e -> {
 
 			contentPanel.removeAll(); // Rénitialise le contenu du contentPanel = page vide
@@ -121,6 +122,8 @@ public class ManagementSoftware {
 
 		// ==============================================================
 
+		// Onglet des produits
+
 		products.addActionListener(e -> {
 
 			contentPanel.removeAll();
@@ -128,61 +131,73 @@ public class ManagementSoftware {
 			ProductsTab.loadProductsTab(contentPanel, frame);
 
 		});
-		
+
 		// ==============================================================
 
-		sales.addActionListener(e -> {
+		// !!! LE MODE VENTE SERA DANS L'ONGLET INVENTAIRE
+		// POURQUOI ? ÉVITER LA REDONDANCE -> POUR LE MODE CAISSE IL FAUT UN TABLEAU
+		// AVEC L'INVENTAIRE DES LOTS DE PRODUITS DISPO EN MAGASIN -> C'EST CE QUE FAIT
+		// L'ONGLET INVENTAIRE DONC AUTANT EN PROFITER POUR RAJOUTER LA FONCTIONNALITÉ
+		// CAISSE À COTÉ ET EN MÊME TEMPS SUPPRIMER UN ONGLET !!!
 
-			contentPanel.removeAll();
+		/*
+		 * sales.addActionListener(e -> {
+		 * 
+		 * contentPanel.removeAll();
+		 * 
+		 * SalesTab.loadSalesTab(contentPanel, frame);
+		 * 
+		 * });
+		 */
 
-			SalesTab.loadSalesTab(contentPanel, frame);
-			
-		});
-		
 		// ==============================================================
+
+		// Onglet des contrats
 
 		contracts.addActionListener(e -> {
 
 			contentPanel.removeAll();
-			
+
 			ContractsTab.loadContractsTab(contentPanel, frame);
 
 		});
-		
+
 		// ==============================================================
+
 		// Onglet des commandes
-		
+
 		orders.addActionListener(e -> {
 
 			contentPanel.removeAll();
 
 			OrdersTab.loadOrdersTab(contentPanel, frame);
-			
+
 		});
-		
+
 		// ==============================================================
-		// Onglet des stats
+
+		// Onglet des statistiques = tableau de bord
 
 		stats.addActionListener(e -> {
 
 			contentPanel.removeAll(); // Supprime tous les composants = page vide
 
 			StatsTab.loadStatsTab(contentPanel, frame);
-			
+
 		});
-		
+
 		// ==============================================================
 
-		// Quand on clique sur l'onglet des paramètres
+		// Onglet des paramètres
 
 		parameters.addActionListener(e -> {
 
 			contentPanel.removeAll(); // Supprime tous les composants = page vide
 
 			ParametersTab.loadParametersTab(contentPanel, frame);
-			
+
 		});
-		
+
 		// ==============================================================
 
 		// Ajouter la sidebar et le contenu principal à la frame
@@ -193,50 +208,54 @@ public class ManagementSoftware {
 		frame.setVisible(true); // Rendre la fenêtre visible
 
 	};
-	
+
+	// ==============================================================
+
 	/**
 	 * @brief permet de clear l'affichage de l'application
 	 * 
-	 * @details à utiliser après la connection d'un utilisateur pour éviter de dupliquer certain item;
+	 * @details à utiliser après la connection d'un utilisateur pour éviter de
+	 *          dupliquer certain item;
 	 */
 	public static void clearApp() {
 		sidebar = null;
 		frame = null;
 	}
-	
+
 	/**
-	 * @brief Créé un bouton avec les caractéristique de la sidebar et l'ajoute automatiquement à la sidebar
+	 * @brief Créé un bouton avec les caractéristique de la sidebar et l'ajoute
+	 *        automatiquement à la sidebar
 	 * 
 	 * @param text
 	 * @return
 	 */
 	public static JButton getSidebarButton(String text) {
 		JButton button = new JButton(text);
-		
-		//caractèristiques du bouton
+
+		// caractèristiques du bouton
 		Dimension bDim = new Dimension(ManagementSoftware.getSidebar().getPreferredSize().width, 40);
 		button.setMaximumSize(bDim);
 		button.setBackground(Palette.BUTTON_DARK_ACTIVE);
 		button.setBorderPainted(false);
-		
-		//on ajoute le boutton à la sidebar
+
+		// on ajoute le boutton à la sidebar
 		ManagementSoftware.getSidebar().add(button);
-		
+
 		return button;
 	}
-	
+
 	public static JLabel getSidebarText(String text) {
 		JLabel label = new JLabel(text);
-		
-		//caractèristiques du label
+
+		// caractèristiques du label
 		label.setFont(new Font("Arial", Font.BOLD, 20));
 		label.setForeground(Palette.TEXT_LIGHT);
-		
-	    ManagementSoftware.getSidebar().add(label);
-		
+
+		ManagementSoftware.getSidebar().add(label);
+
 		return label;
 	}
-	
+
 	/**
 	 * @brief Créé la sidebar
 	 * 
@@ -244,16 +263,16 @@ public class ManagementSoftware {
 	 */
 	public static JPanel getSidebar() {
 		// Création de la barre latérale
-		if(sidebar == null) {
+		if (sidebar == null) {
 			sidebar = new JPanel();
 			sidebar.setLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
 			sidebar.setPreferredSize(new Dimension(300, ManagementSoftware.getFrame().getHeight()));
 			sidebar.setBackground(Palette.BACKGROUND_DARK_SIDEBAR); // Vert menthe
 		}
-		
+
 		return sidebar;
 	}
-	
+
 	/**
 	 * @brief créé la frame
 	 * 
@@ -261,13 +280,13 @@ public class ManagementSoftware {
 	 */
 	public static JFrame getFrame() {
 		// Création de la fenêtre
-		if(frame == null) {
+		if (frame == null) {
 			frame = new JFrame("AMS - Logiciel de gestion");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(1920,1080);
+			frame.setSize(1920, 1080);
 		}
-		
+
 		return frame;
 	}
-	
+
 };
